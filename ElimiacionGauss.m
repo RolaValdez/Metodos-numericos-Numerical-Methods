f1ef9e3defbbf2a2551341d7ac1f6ac460ba150f
Rolando Valdez Guzmán <66616861+RolaValdez@users.noreply.github.com>
2021-09-12 01:37:31 -0700

    Add files via upload

---
+++ ElimiacionGauss.m
@@ -0,0 +1,76 @@
+function x = ElimiacionGauss(A,B)
+%Autor: Rolando Valdez Guzmán
+%Alias: Tutoingeniero
+%Canal de Youtube: https://www.youtube.com/channel/UCU1pdvVscOdtLpRQBp-TbWg
+%Versión: 2.0
+%Actualizado: 3/sep/2021
+
+% ESTA FUNCION PIDE LOS SIGUIENTES DATOS DE ENTRADA:
+
+% A = matriz cuadrada de coeficientes del sistema de ecuaciones.
+% B = Vector columna de resultados de cada ecuación.
+
+% VARIABLES DE SALIDA:
+
+% x = vector con los valore para todas las variables del sistema de
+% ecuaciones.
+
+%~~~~~~~~~~~~~~~Protección contra errores en las entradas~~~~~~~~~~~~~~~~~%
+if nargin ~= 2                 
+    error('Se debe ingresar una matriz cuadrada A y un vector columna B');
+%Si se ingresan todos los datos de entrada, elegir un método de solución
+else                          
+    if size(A,1) ~= size(A,2)
+        error('Se necesita que la matriz A sea cuadrada')
+    elseif size(B,2) ~= 1
+        error('B debe ser un vector columna');
+    elseif size(A,1) ~= size(B,1)
+        error('El número de filas de A no coincide con el de B. Sistema inconsistente');
+    end
+end
+
+disp(newline);
+if prod(diag(A)) == 0
+    error('El determinante de la matriz A es cero, no se puede resolver');
+end
+
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Setup~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
+
+n = size(A,1); t = ' | '; T = repmat(t,n,1);
+a = num2str(A); b = num2str(B); c = [a T b]; %%unión de los datos en una solo matriz
+disp('Sistema original'); disp(c); disp(newline);
+
+%~~~~~~~~~~~~~~~~~~~~~~Eliminación hacia adelante~~~~~~~~~~~~~~~~~~~~~~~~~%
+
+j = 1;
+for k = 1:n - 1
+    for i = k + 1:n
+        factor = A(i,k)/A(k,k);
+        A(i,:) = A(i,:) - factor*A(k,:);
+        B(i) = B(i) - factor*B(k);
+        c = [num2str(A), T, num2str(B)]; %%unión de los datos en una solo matriz
+        disp(['Paso ',num2str(j)]); disp(c); disp(newline);
+        j = j+1;
+    end
+end
+
+%~~~~~~~~~~~~~~~~~~~~~~~~~Sustitución hacia atrás~~~~~~~~~~~~~~~~~~~~~~~~~%
+
+x(n) = B(n)/A(n,n);
+for i = n - 1:-1:1
+    sum = B(i);
+    for j = i + 1:n
+        sum = sum - A(i,j)*x(j);
+    end
+    x(i) = sum/A(i,i);
+end
+x = x';
+
+%~~~~~~~~~~~~~~~~~~~~~~~~~Impresión de resultados~~~~~~~~~~~~~~~~~~~~~~~~~%
+
+disp('Resultados');
+for i = 1:n
+    fprintf('x%d = %f',i,x(i));
+    fprintf('\n');
+end
+
