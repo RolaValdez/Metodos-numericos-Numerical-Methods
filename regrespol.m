625ebdc3c992c6480c9b985f499bad661317c9f0
Rolando Valdez Guzmán <66616861+RolaValdez@users.noreply.github.com>
2022-05-22 23:07:55 -0700

    Update regrespol.m

--- regrespol.m
+++ regrespol.m
@@ -1,21 +1,22 @@
-function [a,Y,T] = regreslineal(x,y,X)
+function [a,Y,T] = regrespol(x,y,m,X)
 %Autor: Rolando Valdez Guzmán
 %Alias: Tutoingeniero
 %Canal de Youtube: https://www.youtube.com/channel/UCU1pdvVscOdtLpRQBp-TbWg
 %Versión: 1.0
-%Actualizado: 31/dic/2021
+%Actualizado: 15/may/2022
 
 % ESTA FUNCION PIDE LOS SIGUIENTES DATOS DE ENTRADA:
 
 % x = Vector de 1 hasta n cantidad de datos.
 % y = Vector que representa los valores de Y para cada valor de x.
+% m = Grado del polinomio de regresión
 % X = Valor a evaluar en la recta de regresión lineal. Si no se desea
       %evaluar un valor en la recta se puede ingresar un vector vacío así: []
 
 % VARIABLES DE SALIDA:
 
-% a = Coeficientes de la recta de regresión: [a1,a0]
-% Y = Si se ingresó un valor para X, Y entrega el valor de la recta de
+% a = Coeficientes de la recta de regresión: [a0,a1,a2,...,a_n]
+% Y = Si se ingresó un valor para X, Y entrega el valor de la función de
       %regresión evaluada en X.
 % T = Tabla con las siguientes variables estadísticas calculadas:
 
@@ -27,26 +28,45 @@ function [a,Y,T] = regreslineal(x,y,X)
     %cdet = Coeficiente de determinación (r^2)
     %ccor = Coeficiente de correlación (r)
 
-%En un ajuste perfecto, Sr = 0 y r = r2 = 1, significa que la línea explica
+%En un ajuste perfecto, Sr = 0 y r = r2 = 1, significa que la función explica
 %el 100% de la variabilidad de los datos. Si r = r2 = 0, Sr = St el ajuste
 %no representa alguna mejora. 
 %(Chapra S, Métodos Numéricos para Ingenieros,5ta edición)
 
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Algoritmo~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
 
-n = length(x);
-sigmaX = sum(x); sigmaX2 = sum(x.^2);
-sigmaY = sum(y); sigmaXY = sum(x.*y);
+n = length(x); 
+if n < m+1
+    error('No es posible realizar una regresión de este orden');
+end
 
-A = [n, sigmaX; sigmaX, sigmaX2];
-B = [sigmaY; sigmaXY];
-a = A\B;
+%Crear el sistema de ecuaciones de m+1 x m+1 dinámicamente
+A = zeros(m+1,m+1); B = zeros(1,m+1); k = 0;
+for i = 1:m+1
+    for j = 1:m+1
+        A(i,j) = sum(x.^((j-1)+k));
+    end
+    B(i) = sum(y.*x.^k);
+    k = k + 1;
+end
+a = A\B'; %Resolver el sistema y obtener los coeficientes del polinomio
 
-Sr = sum((y - a(1) - a(2).*x).^2);
-St = sum((y - sigmaY/n).^2);
-es = sqrt(Sr/(n-2));
-cdet = (St - Sr)/St;
+%Evaluar el polinomio con todos los valores de X y guardarlos en una matriz
+ymodelo = zeros(n,m+1); k = 0;
+for j = 1:m+1
+    ymodelo(:,j) = a(j).*x.^k;
+    k = k+1;
+end
+ymodelo = sum(ymodelo,2);
+Sr = sum((y - ymodelo').^2); %Regresión por mínimos cuadrados
+
+%Otros datos estadísticos
+St = sum((y - sum(y)/n).^2);
+es = sqrt(Sr/(n-(m+1)));
+cdet = ((St - Sr)/St);
 ccor = sqrt(cdet);
+
+%Tabla de resultados
 E1 = {'Sr';'St';'Error estándar del estimado';...
       'Coeficiente de determinación';'Coeficiente de correlación'};
 E2 = [Sr; St; es; cdet; ccor];
@@ -54,25 +74,31 @@ T = table(E1,E2,'VariableNames',{'Valores calculados','Resultados'});
 
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Gráfica~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
 
-scatter(x,y,'LineWidth',1,'MarkerEdgeColor','b'); hold on
+%PARA USAR ESTA SECCION DEL CODIGO SE NECESITA LA SYMBOLIC MATH TOOLBOX
+%DE LO CONTRARIO, HAY QUE BORRAR TODO ESTO
+scatter(x,y,'LineWidth',2,'MarkerEdgeColor','b'); hold on
 modelo = poly2sym(flipud(a));
 f = matlabFunction(modelo);
-fplot(modelo,[x(1),x(end)],'LineWidth',2,'Color','k'); hold on
+fplot(modelo,[x(1),x(end)],'LineWidth',2); hold on
 grid on
-if sign(a(2)) == -1
-    title(['Y = ',num2str(a(1)),num2str(a(2)),'x']);
-else
-    title(['Y = ',num2str(a(1)),' + ',num2str(a(2)),'x']);
+titlestr{1} = num2str(a(1));
+for i = 2:m+1
+    if sign(a(i)) == 1
+        titlestr{i} = [' + ',num2str(a(i)),'x^',num2str(i-1)];
+    elseif sign(a(i)) == -1
+        titlestr{i} = [' ',num2str(a(i)),'x^',num2str(i-1)];
+    end
 end
-maxX = max(x); 
+S = cell2mat(titlestr); title(['Y = ',S]);
+maxX = max(x); minX = min(x);
 maxY = max(y); minY = min(y);
-axis([0, maxX + 2, minY - 2, maxY + 2]);
+axis([minX, maxX + 2, minY - 2, maxY + 2]);
 h = gcf; h.Position(1:2) = [765,90];
 
-if nargin < 3
+if nargin < 4
     Y = NaN;
     return
-elseif nargin == 3
+elseif nargin == 4
     Y = f(X);
     plot(X,Y,'ro','MarkerFaceColor','r');
 end
